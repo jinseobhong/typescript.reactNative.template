d4b7046fc5e675bb5f432680eab20cd403e503a6
jinseobhong <jinseobhong0427@gmail.com>
2021-06-21 21:41:14 +0900

    Fix Where the location of project.exe.react.cliPath is not recognized during building with Gradle ( #30 ) Add key-value pair about project.exe.react.cliPath to project.ext.react defined in android/app/build.gradle of sourceDir defined in react-native.config.js

--- react-native-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    project: {
-        android: {
-            sourceDir: './__builders__/android',
-        },
-    },
-}
---
+++ react-native.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    project: {
+        ext: {
+            react: {
+                cliPath: '../../node_modules/react-native/cli.js'
+            }
+        },
+        android: {
+            sourceDir: './__builders__/android',
+        }
+    }
+}
